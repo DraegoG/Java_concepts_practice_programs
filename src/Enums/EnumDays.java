27cd34227bc75a7234c67ed4f9799bbd208c2aae
agaur.lab <agaur@opentext.com>
2025-03-02 19:45:43 +0530

    added enums

---
+++ src/Enums/EnumDays.java
@@ -0,0 +1,5 @@
+package Enums;
+
+public enum EnumDays {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+}
---
+++ src/Enums/EnumPractice.java
@@ -0,0 +1,10 @@
+package Enums;
+
+public class EnumPractice {
+    public static void main(String[] args) {
+        System.out.println("Using enums");
+
+        EnumDays today = EnumDays.MONDAY;
+        System.out.println("Today is:" + today);
+    }
+}
