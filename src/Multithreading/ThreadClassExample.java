71ce109b41908d991e0793b95e368e431f9dc73e
agaur.lab <agaur@opentext.com>
2025-05-01 16:57:03 +0530

    added thread and thread communication

--- src/Multithreading/ThreadClassExample.java
+++ src/Multithreading/ThreadClassExample.java
@@ -3,7 +3,13 @@ package Multithreading;
 public class ThreadClassExample {
     public static void main(String[] args) {
         ThreadExtend t1 = new ThreadExtend();
+        Thread t2 = new Thread(() ->
+        {
+            System.out.println("Running from thread object t2");
+        }
+        );
         t1.start();
+        t2.start();
     }
 }
 
