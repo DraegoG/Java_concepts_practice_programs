52d06449647c3dec55a0eccf594d84e3f044071f
agaur.lab <agaur@opentext.com>
2025-05-01 16:58:48 +0530

    added ExtendAbstractExample

--- src/AbstractClassesAndInterfaces/ForUnderstandignAndTryOut/ExtendAbstractExample.java
+++ src/AbstractClassesAndInterfaces/ForUnderstandignAndTryOut/ExtendAbstractExample.java
@@ -2,6 +2,20 @@ package AbstractClassesAndInterfaces.ForUnderstandignAndTryOut;
 
 public class ExtendAbstractExample extends AbstractClassExample{
 
+
+
+    public static void main(String[] args) {
+        AbstractClassExample ab = new AbstractClassExample() {
+            @Override
+            void abstractMethod1() {
+
+            }
+        };
+
+
+    }
+
+    //compulsory to implement the method in child class
     @Override
     void abstractMethod1() {
 
